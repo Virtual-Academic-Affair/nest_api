a983da7e5b9eab93f30b3ef9be7e018fe6675c6e
manhnd <manhnd@smartgift.vn>
2025-11-17 12:36:48 +0700

    google authentication

--- src/exampleResource/dto/update-exampleResource.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateExampleresourceDto } from './create-exampleResource.dto';
-
-export class UpdateExampleResourceDto extends PartialType(
-  CreateExampleresourceDto,
-) {}
