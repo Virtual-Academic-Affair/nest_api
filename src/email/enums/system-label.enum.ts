58f69a9af9678d52fcd10b569b6c62a2b4a227f1
manhnd <manhnd@smartgift.vn>
2025-12-07 11:46:57 +0700

    email

---
+++ src/email/enums/system-label.enum.ts
@@ -0,0 +1,11 @@
+export enum SystemLabel {
+  ClassRegistration = 'class_registration',
+  Administrative = 'administrative',
+  Department = 'department',
+  Inquiry = 'inquiry',
+  Graduation = 'graduation',
+  Academic = 'academic',
+  Other = 'other',
+}
+
+export const SYSTEM_LABEL_KEYS = Object.values(SystemLabel);
